1462d5925d7a343c30d21c390ae043a41543bf31
Sri Harsha Chilakapati <hello@goharsha.com>
2018-01-25 21:41:00 +0530

    Added a test NGM project and load it from player

--- NaaGamePlayerCore/src/main/java/com/naagame/player/NaaGamePlayer.java
+++ NaaGamePlayerCore/src/main/java/com/naagame/player/NaaGamePlayer.java
@@ -1,22 +1,43 @@
 package com.naagame.player;
 
+import com.naagame.core.NgmProject;
+import com.naagame.core.io.ProjectReader;
+import com.shc.easyjson.ParseException;
 import com.shc.silenceengine.core.Game;
 import com.shc.silenceengine.core.SilenceEngine;
 import com.shc.silenceengine.input.Keyboard;
+import com.shc.silenceengine.io.FilePath;
+import com.shc.silenceengine.logging.Logger;
 
-public class NaaGamePlayer extends Game
-{
+public class NaaGamePlayer extends Game {
+    public static Logger logger;
+
     @Override
-    public void init()
-    {
+    public void init() {
+        logger = SilenceEngine.log.getLogger("NaaGamePlayer");
+
         SilenceEngine.display.setTitle("NaaGamePlayer: SilenceEngine " + SilenceEngine.getVersionString());
-        SilenceEngine.display.setSize(1280, 720);
         SilenceEngine.display.centerOnScreen();
+
+        SilenceEngine.io.getFileReader()
+                .readTextFile(FilePath.getResourceFile("test.ngm"))
+                .then(this::projectLoaded);
+    }
+
+    private void projectLoaded(String json) {
+        try {
+            ProjectReader.loadFromJSON(json);
+            NgmProject.textures.forEach(texture -> {
+                logger.info("Texture: " + texture.getName());
+                logger.info("file: " + texture.getFileName());
+            });
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void update(float deltaTime)
-    {
+    public void update(float deltaTime) {
         if (Keyboard.isKeyTapped(Keyboard.KEY_ESCAPE))
             SilenceEngine.display.close();
     }
