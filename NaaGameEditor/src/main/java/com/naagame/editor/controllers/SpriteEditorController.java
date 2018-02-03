563462db39adfc5385023039757260fb32d07960
Sri Harsha Chilakapati <hello@goharsha.com>
2018-02-03 10:52:47 +0530

    Preview sprite animation in SpriteEditor

--- NaaGameEditor/src/main/java/com/naagame/editor/controllers/SpriteEditorController.java
+++ NaaGameEditor/src/main/java/com/naagame/editor/controllers/SpriteEditorController.java
@@ -4,6 +4,12 @@ import com.naagame.core.NgmProject;
 import com.naagame.core.resources.IResource;
 import com.naagame.core.resources.NgmSprite;
 import com.naagame.core.resources.NgmTexture;
+import com.naagame.editor.util.ImageUtil;
+import com.naagame.editor.util.ImageViewer;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,33 +17,40 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TitledPane;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.image.Image;
+import javafx.util.Duration;
 import javafx.util.StringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class SpriteEditorController implements IController, Initializable {
-    @FXML TableView<NgmSprite.Frame> framesView;
-    @FXML TableColumn<NgmSprite.Frame, String> textureColumn;
-    @FXML TableColumn<NgmSprite.Frame, Integer> durationColumn;
+    private @FXML TableView<NgmSprite.Frame> framesView;
+    private @FXML TableColumn<NgmSprite.Frame, String> textureColumn;
+    private @FXML TableColumn<NgmSprite.Frame, Integer> durationColumn;
+    private @FXML TitledPane previewPane;
 
     private ObservableList<String> textures;
     private ObservableList<NgmSprite.Frame> frames;
     private NgmSprite currentSprite;
+    private ImageViewer imageViewer;
+
+    private Timeline timeline;
 
     @Override
     public void init(String name) {
         currentSprite = NgmProject.find(NgmProject.sprites, name);
 
-        textures.clear();
-        textures.addAll(NgmProject.textures
-                .stream().map(IResource::getName).collect(Collectors.toList()));
-
         frames.clear();
         frames.addAll(currentSprite.getFrames());
+
+        resourcesChanged();
+        updateAnimation();
     }
 
     @FXML
@@ -47,6 +60,7 @@ public class SpriteEditorController implements IController, Initializable {
         }
 
         frames.add(new NgmSprite.Frame(NgmProject.textures.get(0), 250));
+        Platform.runLater(this::updateAnimation);
     }
 
     @FXML
@@ -58,10 +72,33 @@ public class SpriteEditorController implements IController, Initializable {
         }
 
         frames.remove(selected);
+        Platform.runLater(this::updateAnimation);
+    }
+
+    private void updateAnimation() {
+        timeline.stop();
+
+        AtomicInteger frame = new AtomicInteger(0);
+        timeline.getKeyFrames().clear();
+
+        timeline.getKeyFrames().addAll(frames.stream().map(f -> {
+            Image image = ImageUtil.loadImage(f.getTexture().getSource());
+            Duration duration = Duration.millis(f.getDuration() * frame.addAndGet(1));
+
+            return new KeyFrame(duration, e -> imageViewer.setImage(image));
+        }).collect(Collectors.toList()));
+
+        timeline.play();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        imageViewer = new ImageViewer();
+        previewPane.setContent(imageViewer);
+
+        timeline = new Timeline();
+        timeline.setCycleCount(Animation.INDEFINITE);
+
         textures = FXCollections.observableArrayList();
         frames = FXCollections.observableArrayList();
 
@@ -71,8 +108,11 @@ public class SpriteEditorController implements IController, Initializable {
             return new SimpleObjectProperty<>(texture == null ? "" : texture.getName());
         });
         textureColumn.setCellFactory(ComboBoxTableCell.forTableColumn(textures));
-        textureColumn.setOnEditCommit(event -> event.getRowValue()
-                .setTexture(NgmProject.find(NgmProject.textures, event.getNewValue())));
+        textureColumn.setOnEditCommit(event -> {
+            event.getRowValue()
+                    .setTexture(NgmProject.find(NgmProject.textures, event.getNewValue()));
+            Platform.runLater(this::updateAnimation);
+        });
 
         durationColumn.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getDuration()));
         durationColumn.setCellFactory(TextFieldTableCell.forTableColumn(new StringConverter<Integer>() {
@@ -87,8 +127,20 @@ public class SpriteEditorController implements IController, Initializable {
             }
         }));
 
-        durationColumn.setOnEditCommit(event -> event.getRowValue().setDuration(event.getNewValue()));
+        durationColumn.setOnEditCommit(event -> {
+            event.getRowValue().setDuration(event.getNewValue());
+            Platform.runLater(this::updateAnimation);
+        });
 
         framesView.setItems(frames);
+    }
+
+    @Override
+    public void resourcesChanged() {
+        textures.clear();
+        textures.addAll(NgmProject.textures
+                .stream().map(IResource::getName).collect(Collectors.toList()));
+
+        Platform.runLater(this::updateAnimation);
     }
 }
