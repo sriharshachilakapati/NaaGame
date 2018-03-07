de6c33aaf8b941bc277f09d9f9247c75b21741d4
Sri Harsha Chilakapati <hello@goharsha.com>
2018-03-07 15:34:13 +0530

    Resizeable canvas in the Scene Editor

--- NaaGameEditor/src/main/java/com/naagame/editor/controllers/SceneEditorController.java
+++ NaaGameEditor/src/main/java/com/naagame/editor/controllers/SceneEditorController.java
@@ -1,7 +1,34 @@
 package com.naagame.editor.controllers;
 
-public class SceneEditorController extends Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SceneEditorController extends Controller implements Initializable {
+
+    @FXML private Canvas sceneCanvas;
+
+    @FXML private Spinner<Integer> spinnerWidth;
+    @FXML private Spinner<Integer> spinnerHeight;
+
     @Override
     public void init(String resourceName) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        spinnerHeight.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 50000, 800));
+        spinnerWidth.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 50000, 600));
+
+        spinnerWidth.valueProperty().addListener((ov, o, n) -> sceneCanvas.setWidth(n));
+        spinnerHeight.valueProperty().addListener((ov, o, n) -> sceneCanvas.setHeight(n));
+
+        spinnerWidth.focusedProperty().addListener((ov, o, n) -> spinnerWidth.increment(0));
+        spinnerHeight.focusedProperty().addListener((ov, o, n) -> spinnerHeight.increment(0));
     }
 }
