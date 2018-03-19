9fe6f841769ce67d107dd68a9e924e8e587bf07f
Sri Harsha Chilakapati <hello@goharsha.com>
2018-03-19 09:32:54 +0530

    Show exceptions in an alert dialog box

--- NaaGameEditor/src/main/java/com/naagame/editor/Main.java
+++ NaaGameEditor/src/main/java/com/naagame/editor/Main.java
@@ -3,10 +3,17 @@ package com.naagame.editor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Objects;
 
 public class Main extends Application {
@@ -19,6 +26,40 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         window = stage;
+
+        Thread.setDefaultUncaughtExceptionHandler((thread, exception) -> {
+            exception.printStackTrace();
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Exception in Program");
+            alert.setHeaderText(String.format("An error occurred while running the previous operation in thread %s...", thread));
+            alert.setContentText(exception.getMessage());
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            exception.printStackTrace(pw);
+            String exceptionText = sw.toString();
+
+            Label label = new Label("The exception stacktrace was:");
+
+            TextArea textArea = new TextArea(exceptionText);
+            textArea.setEditable(false);
+            textArea.setWrapText(true);
+
+            textArea.setMaxWidth(Double.MAX_VALUE);
+            textArea.setMaxHeight(Double.MAX_VALUE);
+            GridPane.setVgrow(textArea, Priority.ALWAYS);
+            GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+            GridPane expContent = new GridPane();
+            expContent.setMaxWidth(Double.MAX_VALUE);
+            expContent.add(label, 0, 0);
+            expContent.add(textArea, 0, 1);
+
+            alert.getDialogPane().setExpandableContent(expContent);
+
+            alert.showAndWait();
+        });
 
         Pane pane = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader()
                 .getResource("main.fxml")));
